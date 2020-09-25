cd6c6c7a2253d13f3c2a3144fde74d3b264dd915
Hobbledehoy <15384813+chelyabinsk@users.noreply.github.com>
2020-09-25 15:55:28 +0100

    cron update

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "* * * * * cd $PWD/RecordData && $(which python3) check_run_status.py >> $PWD/cron.log 2>&1" >> mycron
-echo "00 09 * * 1-5 echo hi" >> mycron
+echo "* * * * * cd $PWD/RecordData && $(which python3) check_run_status.py >> $PWD/cron_scraper.log 2>&1" >> mycron
+echo "5 2 * * * cd $PWD && $(which python3) updater.py >> $PWD/cron_updater.log 2>&1" >> mycron
 #install new cron file
 crontab mycron
 rm mycron*
--- updater.py
+++ updater.py
@@ -19,22 +19,29 @@ def new_version():
     return new_json
 
 def local_version():
-    with open('../version.info','r') as f:
+    with open('version.info','r') as f:
         return json.loads(f.read())
     
 def create_version():
     data = {'version':2,'newfiles':[{'localfile':'hello.txt','remotefile':'https://raw.githubusercontent.com/chelyabinsk/RemoteWork/master/Makefile'}]}
-    with open('../version.info','w') as f:
+    with open('version.info','w') as f:
         f.write(json.dumps(data))
-create_version()
-#new_files = new_version()
-#local_files = local_version()
+#create_version()
+new_files = new_version()
+local_files = local_version()
 
-#if new_files['version'] >= local_files['version']:
-#    for file in new_files['newfiles']:
-#        data = urlopen(file['remotefile'])
-#        with open('localfile','w') as f:
-#            for line in data:
-#                decoded_line = line.decode('utf-8')
-#                f.write(decoded_line)
+if new_files['version'] > local_files['version']:
+    # Download all of the new files
+    for file in new_files['newfiles']:
+        data = urlopen(file['remotefile'])
+        with open(file['localfile'],'w') as f:
+            for line in data:
+                decoded_line = line.decode('utf-8')
+                f.write(decoded_line)
+    # Update the version.info file
+    data = urlopen(base_url + 'version.info')
+    with open('version.info','w') as f:
+        for line in data:
+            decoded_line = line.decode('utf-8')
+            f.write(decoded_line)
         
