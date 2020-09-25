b4b99e41c9c8e8eaaa9fae44bbb450cd66d936e0
Hobbledehoy <15384813+chelyabinsk@users.noreply.github.com>
2020-09-25 15:01:33 +0100

    version update

--- RecordData/updater.py
+++ RecordData/updater.py
@@ -8,4 +8,19 @@ If new version is found on github then referenced files are replaced in the fold
 """
 
 import requests
+import json
 
+base_url = 'https://raw.githubusercontent.com/chelyabinsk/RemoteWork/master/'
+
+def new_version():
+    r = requests.get(base_url+'version.info')
+    print(r.text)
+    
+def create_version():
+    version = {'version':2}
+    changed_files = [{'none':'none'}]
+    data = {'data':[version,changed_files]}
+    with open('version.info','w') as f:
+        f.write(json.dumps(data))
+#new_version()
+create_version()
