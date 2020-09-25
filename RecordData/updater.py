9a96789de409f02096964a99c547493483c67b1d
Hobbledehoy <15384813+chelyabinsk@users.noreply.github.com>
2020-09-25 15:16:55 +0100

    version update

--- RecordData/updater.py
+++ RecordData/updater.py
@@ -16,13 +16,19 @@ def new_version():
     r = requests.get(base_url+'version.info')
     new_json = r.json()
     return new_json
+
+def local_version():
+    with open('../version.info','r') as f:
+        return json.loads(f.read())
     
 def create_version():
 #    version = {'version':2}
 #    changed_files = {'newfiles':[{'none':'none'}]}
-    data = {'version':2,'newfiles':[[{'none':'none'}]]}
+    data = {'version':2,'newfiles':[{'none':'none'}]}
     with open('../version.info','w') as f:
         f.write(json.dumps(data))
-#a= new_version()
+a= new_version()
 create_version()
+b = local_version()
+
         
