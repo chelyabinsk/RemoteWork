e127ccf62823889b0134dc678d9013ca082b605e
Hobbledehoy <15384813+chelyabinsk@users.noreply.github.com>
2020-09-29 18:55:35 +0100

    Update check_run_status.py

--- RecordData/check_run_status.py
+++ RecordData/check_run_status.py
@@ -9,6 +9,12 @@ import os
 import datetime
 from uploader import make_ping,upload_youtube_data
 
+now = datetime.datetime.now()
+
+if now.minute % 15 == 0:
+    make_ping()
+if now.hour == 2 and now.minute == 4:
+    upload_youtube_data()
 
 Popen(['nohup', '../install.sh'], stdout=DEVNULL, stderr=DEVNULL)
 
@@ -26,12 +32,5 @@ if do_run:
     Popen(['nohup', '/opt/anaconda3/bin/python3' ,'find_steams.py'], stdout=DEVNULL, stderr=DEVNULL)
 #    pass
 #    Popen(['nohup', '/usr/bin/python3' ,'hi.py'], stdout=DEVNULL, stderr=DEVNULL)
-    
-now = datetime.datetime.now()
-
-if now.minute % 15 == 0:
-    make_ping()
-if now.hour == 2 and now.minute == 4:
-    upload_youtube_data()
-    
+        
 exit()        
