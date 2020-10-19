90aa74c872c6c4c60958303da0b49cf8e82e7146
echomsk <72651265+echomsk@users.noreply.github.com>
2020-10-19 12:15:48 +0100

    v13

--- RecordData/data_fix.py
+++ RecordData/data_fix.py
@@ -13,21 +13,21 @@ import glob
 from pathlib import Path
 
 def gen_file(repo):
-	todaydate = datetime.datetime.today().strftime('%Y-%m-%d')
-	df = pd.read_csv(glob.glob('viewers/*.txt')[0],sep="\t")
-	sub_df = df.loc[(df['live']==True) ,['timestamp','viewers','channel_name']]
-	#sub_df = df[['timestamp','viewers','channel_name']]
-	sub_df[['timestamp']] = pd.to_datetime(sub_df['timestamp']+60*60*2,unit='s')
-	sub_df = sub_df.sort_values('timestamp')
-	tmp = list(sub_df['timestamp'])[0].date()
-	year = tmp.year
-	month = str(tmp.month).rjust(2,'0')
-	day = str(tmp.day).rjust(2,'0')
-	out_name = '../../{}/data/{}-{}-{}.csv'.format(repo,year,month,day)
-	export_df = sub_df.pivot_table(values='viewers', index=['timestamp'],columns='channel_name', aggfunc=np.sum)
-	export_df.to_csv(out_name)
-	print(out_name)
-	return out_name,tmp
+        todaydate = datetime.datetime.today().strftime('%Y-%m-%d')
+        df = pd.read_csv(glob.glob('viewers/*.txt')[0],sep="\t")
+        sub_df = df.loc[(df['live']==True) ,['timestamp','viewers','channel_name']]
+        #sub_df = df[['timestamp','viewers','channel_name']]
+        sub_df[['timestamp']] = pd.to_datetime(sub_df['timestamp']+60*60*2,unit='s')
+        sub_df = sub_df.sort_values('timestamp')
+        tmp = list(sub_df['timestamp'])[0].date()
+        year = tmp.year
+        month = str(tmp.month).rjust(2,'0')
+        day = str(tmp.day).rjust(2,'0')
+        out_name = '../../{}/data/{}-{}-{}.csv'.format(repo,year,month,day)
+        export_df = sub_df.pivot_table(values='viewers', index=['timestamp'],columns='channel_name', aggfunc=np.sum)
+        export_df.to_csv(out_name)
+        print(out_name)
+        return out_name,tmp
 
 def update_date(m,d,y,raw_url,repo):
         r = requests.get(raw_url)
@@ -53,11 +53,11 @@ def gh_procedure():
                 raw_url = tmp[4]
 
         try:
-                cp = cmd.run('cd ../..; git clone {}'.format(url),check=True,shell=True)
+            cp = cmd.run('cd ../..; git clone {}'.format(url),check=True,shell=True)
         except:
-                cp = cmd.run('rm ../../{} -rf'.format(repo),check=True,shell=True)
-                cp = cmd.run('cd ../..; git clone {}'.format(url),check=True,shell=True)
-        
+            cp = cmd.run('rm ../../{} -rf'.format(repo),check=True,shell=True)
+            cp = cmd.run('cd ../..; git clone {}'.format(url),check=True,shell=True)
+        #print('../..; git clone {}'.format(url)) 
         a=gen_file(repo)
         year = a[1].year
         month = str(a[1].month).rjust(2,'0')
@@ -69,17 +69,14 @@ def gh_procedure():
         with open('{}/.git-credentials'.format(home),'w') as f:
                 f.write('https://{}:{}@github.com\n'.format(email,token))
         try:
-                #print("add files")
+                print("add files")
                 cmd.run('cd ../../{}; git add .'.format(repo),check=True,shell=True)
-                #print("commit")
+                print("commit")
                 cmd.run('cd ../../{}; git commit -am "{} upload"'.format(repo,todaydate),check=True,shell=True)
-                #print("push")
-                cmd.run("cd ../../{}; git push".format(repo),check=True,shell=True) #<<!
-#{}
-#{}
-#!""".format(repo,email,token),check=True,shell=True)
+                print("push")
+                cmd.run("""cd ../../{}; git push https://{}:{}@github.com/{}/{}.git""".format(repo,email,token,email,repo),check=True,shell=True)
         except:
                 print("oh dear! git fucked up")
-        cp = cmd.run('rm ../../{} -rf'.format(repo),check=True,shell=True)
+        #cp = cmd.run('rm ../../{} -rf'.format(repo),check=True,shell=True)
 
 #gh_procedure()
