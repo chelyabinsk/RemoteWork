8471a78c0b2603accaecc2df827d554056b9dee2
echomsk <72651265+echomsk@users.noreply.github.com>
2020-10-24 14:52:18 +0100

    changes

--- RecordData/hi.py
+++ RecordData/hi.py
@@ -1,13 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep 25 13:25:11 2020
-
-@author: vz237
-"""
-
 from time import sleep
+from data_fix import gh_procedure
 
-with open('hi.txt','a') as f:
-    for i in range(10):
-        f.write('{}\n'.format(i))
-        sleep(0.5)
+#with open('hi.txt','a') as f:
+#    for i in range(10):
+#        f.write('{}\n'.format(i))
+#        sleep(0.5)
+
+gh_procedure()
+
